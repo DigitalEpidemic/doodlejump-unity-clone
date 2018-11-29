2319edb35b56e00a9e9b91a6db094c93da3c0c31
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-11-29 00:57:21 -0500

    Fixed camera smoothing and Doodler animation jitter (Interpolation for both)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
     [SerializeField] Transform target;
-
-    Vector3 currentVelocity;
+    [SerializeField] float smoothTime = 1f;
+    Vector3 velocity;
 	
 	void LateUpdate () {
 		if (target.position.y > transform.position.y) {
             Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
-            transform.position = newPos;
+            //Vector3 smoothPos = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+            Vector3 smoothPos = Vector3.Lerp(transform.position, newPos, smoothTime * Time.deltaTime);
+            transform.position = smoothPos;
         }
 	}
 }
